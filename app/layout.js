0f922e24d71287986687931bedbfab8ce7773557
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-06-17 12:41:27 +0300

    edit the readme file format

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -18,10 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${roboto.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -3,9 +3,182 @@ import { addMovie } from "@/actions/action";
 
 export default function Home() {
   return (
-    <form action={addMovie}>
-      <input type="text" name="name" />
-      <button type="submit">add movie</button>
-    </form>
+    <section
+      className="text-gray-300 body-font overflow-hidden"
+      data--h-bstatus="0OBSERVED"
+    >
+      <div className="container px-5 py-24 mx-auto" data--h-bstatus="0OBSERVED">
+        <div
+          className="-my-8 divide-y-2 divide-gray-100"
+          data--h-bstatus="0OBSERVED"
+        >
+          <div
+            className="py-8 flex flex-wrap md:flex-nowrap"
+            data--h-bstatus="0OBSERVED"
+          >
+            <div
+              className="md:w-64 md:mb-0 mb-6 shrink-0 flex flex-col"
+              data--h-bstatus="0OBSERVED"
+            >
+              <span
+                className="font-semibold title-font text-gray-200"
+                data--h-bstatus="0OBSERVED"
+              >
+                CATEGORY
+              </span>
+              <span
+                className="mt-1 text-gray-400 text-sm"
+                data--h-bstatus="0OBSERVED"
+              >
+                12 Jun 2019
+              </span>
+            </div>
+            <div className="md:grow" data--h-bstatus="0OBSERVED">
+              <h2
+                className="text-2xl font-medium text-gray-100 title-font mb-2"
+                data--h-bstatus="0OBSERVED"
+              >
+                Bitters hashtag waistcoat fashion axe chia unicorn
+              </h2>
+              <p className="leading-relaxed" data--h-bstatus="0OBSERVED">
+                Glossier echo park pug, church-key sartorial biodiesel
+                vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
+                party messenger bag selfies, poke vaporware kombucha
+                lumbersexual pork belly polaroid hoodie portland craft beer.
+              </p>
+              <a
+                className="text-indigo-500 inline-flex items-center mt-4"
+                data--h-bstatus="0OBSERVED"
+              >
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  data--h-bstatus="0OBSERVED"
+                >
+                  <path d="M5 12h14" data--h-bstatus="0OBSERVED"></path>
+                  <path d="M12 5l7 7-7 7" data--h-bstatus="0OBSERVED"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div
+            className="py-8 flex flex-wrap md:flex-nowrap"
+            data--h-bstatus="0OBSERVED"
+          >
+            <div
+              className="md:w-64 md:mb-0 mb-6 shrink-0 flex flex-col"
+              data--h-bstatus="0OBSERVED"
+            >
+              <span
+                className="font-semibold title-font text-gray-200"
+                data--h-bstatus="0OBSERVED"
+              >
+                CATEGORY
+              </span>
+              <span
+                className="mt-1 text-gray-400 text-sm"
+                data--h-bstatus="0OBSERVED"
+              >
+                12 Jun 2019
+              </span>
+            </div>
+            <div className="md:grow" data--h-bstatus="0OBSERVED">
+              <h2
+                className="text-2xl font-medium text-gray-100 title-font mb-2"
+                data--h-bstatus="0OBSERVED"
+              >
+                Meditation bushwick direct trade taxidermy shaman
+              </h2>
+              <p className="leading-relaxed" data--h-bstatus="0OBSERVED">
+                Glossier echo park pug, church-key sartorial biodiesel
+                vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
+                party messenger bag selfies, poke vaporware kombucha
+                lumbersexual pork belly polaroid hoodie portland craft beer.
+              </p>
+              <a
+                className="text-indigo-500 inline-flex items-center mt-4"
+                data--h-bstatus="0OBSERVED"
+              >
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  data--h-bstatus="0OBSERVED"
+                >
+                  <path d="M5 12h14" data--h-bstatus="0OBSERVED"></path>
+                  <path d="M12 5l7 7-7 7" data--h-bstatus="0OBSERVED"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div
+            className="py-8 flex flex-wrap md:flex-nowrap"
+            data--h-bstatus="0OBSERVED"
+          >
+            <div
+              className="md:w-64 md:mb-0 mb-6 shrink-0 flex flex-col"
+              data--h-bstatus="0OBSERVED"
+            >
+              <span
+                className="font-semibold title-font text-gray-200"
+                data--h-bstatus="0OBSERVED"
+              >
+                CATEGORY
+              </span>
+              <span
+                className="text-sm text-gray-400"
+                data--h-bstatus="0OBSERVED"
+              >
+                12 Jun 2019
+              </span>
+            </div>
+            <div className="md:grow" data--h-bstatus="0OBSERVED">
+              <h2
+                className="text-2xl font-medium text-gray-100 title-font mb-2"
+                data--h-bstatus="0OBSERVED"
+              >
+                Woke master cleanse drinking vinegar salvia
+              </h2>
+              <p className="leading-relaxed" data--h-bstatus="0OBSERVED">
+                Glossier echo park pug, church-key sartorial biodiesel
+                vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
+                party messenger bag selfies, poke vaporware kombucha
+                lumbersexual pork belly polaroid hoodie portland craft beer.
+              </p>
+              <a
+                className="text-indigo-500 inline-flex items-center mt-4"
+                data--h-bstatus="0OBSERVED"
+              >
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  data--h-bstatus="0OBSERVED"
+                >
+                  <path d="M5 12h14" data--h-bstatus="0OBSERVED"></path>
+                  <path d="M12 5l7 7-7 7" data--h-bstatus="0OBSERVED"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
